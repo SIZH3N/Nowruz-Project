80669467ba70ad64645be1124a85a67ee4666663
AmirHossein <goodarzizadeh.ah@gmail.com>
2025-04-17 19:06:17 +0330

    comment and suggestions

--- demo/src/main/java/sbu/cs/demo/data/EditSuggestion.java
+++ demo/src/main/java/sbu/cs/demo/data/EditSuggestion.java
@@ -1,4 +1,52 @@
 package sbu.cs.demo.data;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.*;
+import java.util.*;
+
 public class EditSuggestion {
+
+   private static final String jsonFile = "src/main/java/sbu/cs/demo/data/Suggestion.json";
+
+    public static List<Suggestion> readSuggestions() {
+        List<Suggestion> suggestions = new ArrayList<>();
+        try (Reader reader = new FileReader(jsonFile)) {
+            Gson gson = new Gson();
+            suggestions = gson.fromJson(reader, new TypeToken<List<Suggestion>>() {
+            }.getType());
+        } catch (IOException e) {
+            System.err.println("Error reading JSON file: " + e.getMessage());
+        }
+        return suggestions;
+    }
+
+    public static void deleteSuggestion(int ID) {
+        List<Suggestion> suggestions = readSuggestions();
+        for (Suggestion suggestion : suggestions) {
+            if (suggestion.getID() == ID) {
+                suggestions.remove(suggestion);
+            }
+        }
+    }
+
+    public static void addSuggestion(Suggestion newSuggestion) {
+        List<Suggestion> suggestions = readSuggestions();
+        suggestions.add(newSuggestion);
+        writeSuggestions(suggestions);
+    }
+
+    public static void writeSuggestions(List<Suggestion> suggestions ){
+        try (Writer writer = new FileWriter(jsonFile)) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(suggestions, writer);
+            System.out.println("Saved: " + suggestions.size() + " Suggestions");
+        } catch (IOException e) {
+            System.err.println("Error writing JSON file: " + e.getMessage());
+        }
+
+    }
+
 }
+
--- demo/src/main/java/sbu/cs/demo/data/Suggestion.java
+++ demo/src/main/java/sbu/cs/demo/data/Suggestion.java
@@ -1,4 +1,16 @@
 package sbu.cs.demo.data;
 
+import sbu.cs.demo.scene.entrance.User;
+
 public class Suggestion {
+    private int ID;
+    private String user;
+    private String suggestedLyrics;
+    private String sellerLyrics;
+    private String title;
+    private boolean approved;
+
+    public int getID() {
+        return ID;
+    }
 }
